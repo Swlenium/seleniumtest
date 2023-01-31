d928978c31881b70f83b557e2bf288e7712c4931
Swaner <swanerjose@gmail.com>
2023-01-30 23:31:13 -0500

    seleniumtest 1.0

--- src/test/java/webtest.java
+++ src/test/java/webtest.java
@@ -8,35 +8,59 @@ import java.time.Duration;
 
 public class webtest {
     public static void main(String[] args) throws Exception {
-
+        //driver chrome
+        //System.setProperty("webdriver.chrome.driver", "C:/Users/Alquileres/Documents/chromedriver");
         WebDriver driver = new ChromeDriver();
+
+        // Espera implicia
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+        //entrar a la web/
         driver.get("https://www.demoblaze.com/index.html");
+        //Registrarse//
         driver.findElement(By.id("signin2")).click();
-        driver.findElement(By.id("sign-username")).sendKeys("swanerbauf");
-        driver.findElement(By.id("sign-password")).sendKeys("swanerbauf");
+        driver.findElement(By.id("sign-username")).sendKeys("plutus");
+        driver.findElement(By.id("sign-password")).sendKeys("plutus");
         driver.findElement(By.cssSelector("button[onclick='register()']")).click();
+        //esperar
         Thread.sleep(3000);
 
-        Alert alert = driver.switchTo().alert();
-        alert.accept();
+        //Aceptar la alerta
+        String alertText = driver.switchTo().alert().getText();
+        if (alertText.equals("This user already exist.")) {
+            Alert alert = driver.switchTo().alert();
+            alert.accept();
+            Thread.sleep(2000);
+            driver.findElement(By.xpath("(//button[@type='button'][normalize-space()='Close'])[2]")).click();
+            driver.findElement(By.xpath("//a[@id='login2']")).click();
+            Thread.sleep(2000);
+            driver.findElement(By.id("loginusername")).sendKeys("plutus");
+            driver.findElement(By.id("loginpassword")).sendKeys("plutus");
+            driver.findElement(By.xpath("//button[normalize-space()='Log in']")).click();
+            Thread.sleep(2000);
+            driver.findElement(By.xpath("(//a[normalize-space()='Sony vaio i5'])[1]")).click();
+            Thread.sleep(2000);
+            driver.findElement(By.xpath("(//a[normalize-space()='Add to cart'])[1]")).click();
+            driver.findElement(By.xpath("(//a[normalize-space()='Cart'])[1]")).click();
+            driver.findElement(By.xpath("(//button[normalize-space()='Place Order'])[1]")).click();
 
-            driver.findElement(By.id("login2"));
+            driver.findElement(By.className("form-control")).sendKeys("swanerbauf");
+            driver.findElement(By.id("country")).sendKeys("peru");
+            driver.findElement(By.id("city")).sendKeys("lima");
+            driver.findElement(By.id("card")).sendKeys("12345678");
+            driver.findElement(By.id("month")).sendKeys("enero");
+            driver.findElement(By.id("year")).sendKeys("2023");
+            driver.findElement(By.className("confirm btn btn-lg btn-primary")).click();
 
-        driver.findElement(By.id("login2"));
-        driver.findElement(By.id("loginusername")).sendKeys("swanerbauf");
-        driver.findElement(By.id("loginpassword")).sendKeys("swanerbauf");
-        driver.findElement(By.className("card-img-top img-fluid")).click();
-        driver.findElement(By.className("btn btn-success btn-lg")).click();
-        driver.findElement(By.id("cartur")).click();
-        driver.findElement(By.className("btn btn-success")).click();
-        driver.findElement(By.className("form-control")).sendKeys("swanerbauf");
-        driver.findElement(By.id("country")).sendKeys("peru");
-        driver.findElement(By.id("city")).sendKeys("lima");
-        driver.findElement(By.id("card")).sendKeys("12345678");
-        driver.findElement(By.id("month")).sendKeys("enero");
-        driver.findElement(By.id("year")).sendKeys("2023");
-        driver.findElement(By.className("confirm btn btn-lg btn-primary")).click();
+        } else {
+            // hacer otra cosa aquí
+        }
+
+
+
+
+
+
 
 
 
